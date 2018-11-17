9ea97ccde31507c20ea8634db32943f23be7ae64
sanketpatel0512 <32018195+sanketpatel0512@users.noreply.github.com>
2018-11-17 18:00:42 -0500

    Delete project.py

--- project.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-schldata = pd.read_csv("schoolzip.csv", usecols = ['dbn','school_name','boro','graduation_rate',\
-                                                    'attendance_rate','pct_stu_enough_variety',\
-                                                    'college_career_rate','pct_stu_safe','Postcode'])
-schldata = schldata.rename(columns = {'pct_stu_enough_variety':'variety_rate','pct_stu_safe':'safety_rate'})
-schldata = schldata.fillna(schldata.mean())
-print(schldata)
