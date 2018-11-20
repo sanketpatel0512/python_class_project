284e3c52cc317239d7ac239fe0e72d645295f858
sanketpatel0512 <32018195+sanketpatel0512@users.noreply.github.com>
2018-11-20 14:31:50 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,7 +1,11 @@
 import pandas as pd
+#School Data Input
 schldata = pd.read_csv("schoolzip.csv", usecols = ['dbn','school_name','boro','graduation_rate',\
                                                     'attendance_rate','pct_stu_enough_variety',\
                                                     'college_career_rate','pct_stu_safe','Postcode'])
 schldata = schldata.rename(columns = {'pct_stu_enough_variety':'variety_rate','pct_stu_safe':'safety_rate'})
 schldata = schldata.fillna(schldata.mean())
-print(schldata)
+
+#Air Data Input
+airdata = pd.read_csv("Air_Quality.csv")
+print(airdata)
